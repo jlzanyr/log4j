859da027dd197a000d82eff6dd6a11993fa396a5
Prudnikov Sergey <sprudnikov@luxoft.com>
2011-09-23 11:47:06 +0700

    Simple Log4j Configuration Learn how to setup a simple log4j configuration.

---
+++ src/main/java/log4j/App.java
@@ -0,0 +1,22 @@
+package log4j;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static final Logger logger = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        BasicConfigurator.configure();
+        logger.debug("Sample debug message");
+        logger.info("Sample info message");
+        logger.warn("Sample warn message");
+        logger.error("Sample error message");
+        logger.fatal("Sample fatal message");
+    }
+}
