513436d7dccce32a7416f28129377c61d695501b
Prudnikov Sergey <sprudnikov@luxoft.com>
2011-09-23 12:20:09 +0700

    Log4j Configuration Using Properties File Learn how to configure Log4j using properties file.

--- src/main/java/log4j/App.java
+++ src/main/java/log4j/App.java
@@ -1,7 +1,7 @@
 package log4j;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Hello world!
@@ -12,7 +12,8 @@ public class App
     static final Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
-        BasicConfigurator.configure();
+//        BasicConfigurator.configure();
+        PropertyConfigurator.configure("log4j.properties");
         logger.debug("Sample debug message");
         logger.info("Sample info message");
         logger.warn("Sample warn message");
